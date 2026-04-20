4ed76c543c9dbebd5c025ba28734b61343106dd5
JhoeJhl <pinto.12574531@gmail.com>
2026-04-20 00:05:10 -0400

    Portafolio Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+#Proyectos y Tecnologias
+proyectos_data = [
+    {
+        "titulo": "E-Learning Platform",
+        "descripcion": "Sistema tipo Udemy con Laravel y Vue.js.",
+        "tecnologias": ["Laravel", "Vue", "Inertia"],
+        "link": "#"
+    },
+    {
+        "titulo": "Pharmacy Web",
+        "descripcion": "Gestión de inventarios para farmacias locales.",
+        "tecnologias": ["PHP", "MySQL", "JavaScript"],
+        "link": "#"
+    }
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', proyectos=proyectos_data)
+
+
+@app.route('/proyectos')
+def proyectos():
+    return render_template('proyectos.html', proyectos=proyectos_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
